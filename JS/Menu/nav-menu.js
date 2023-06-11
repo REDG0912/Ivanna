f29451be9ded6265104fc6c1aac96109f13f0d8e
Nickolas <skolia2005@gmail.com>
2023-06-11 14:13:33 +0100

    .

--- JS/Menu/nav-menu.js
+++ JS/Menu/nav-menu.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector("header").classList.toggle("open")
-    })
-})
+    var menu = document.querySelector("header");
+    var burger = document.getElementById("burger");
+    document.addEventListener("click", function(event) {
+        var targetElement = event.target;
+        var isClickInsideMenu = menu.contains(targetElement) || targetElement === menu;
+        if (!isClickInsideMenu) {
+            menu.classList.remove("open");
+        }
+    });
+    burger.addEventListener("click", function() {
+        menu.classList.toggle("open");
+    });
+});

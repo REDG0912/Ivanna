4271620b56ce002368178df6289e4c61ef13ad4e
Nickolas <skolia2005@gmail.com>
2023-06-17 15:17:20 +0200

    new page

---
+++ info/JS/Menu/nav-menu.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var menu = document.querySelector("header");
+    var burger = document.getElementById("burger");
+    document.addEventListener("touchstart", function(event) {
+        var targetElement = event.target;
+        var isClickInsideMenu = menu.contains(targetElement) || targetElement === menu;
+        if (!isClickInsideMenu) {
+            menu.classList.remove("open");
+        }
+    });
+    burger.addEventListener("click", function() {
+        menu.classList.toggle("open");
+    });
+});
